dd901a74f9c888a7152ee70deff45c970a0f3f26
Samuel Bouchet <samuel.bouchet@gmail.com>
2018-01-26 22:11:04 +0100

    Car prefab

---
+++ Assets/Scripts/CameraSizeFromRatio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraSizeFromRatio : MonoBehaviour {
+
+	public float height = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Camera.main.orthographicSize = height / Camera.main.aspect;
+		Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, - Camera.main.orthographicSize, Camera.main.transform.position.z);
+	}
+}
