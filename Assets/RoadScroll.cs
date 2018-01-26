be876624d5a70a05c12f6a533452b0acf8e42ed5
Samuel Bouchet <samuel.bouchet@gmail.com>
2018-01-26 23:40:02 +0100

    Vrooooooooom ! Tracks are scrolling

---
+++ Assets/RoadScroll.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RoadScroll : MonoBehaviour {
+
+	public float speed = 0.2f;
+	
+	public GameObject[] tracks;
+
+	private float offset = 0f;
+
+	private GameObject currentTrack = null;
+	private GameObject nextTrack = null;
+
+	private Vector3 startPosCurrent = new Vector3(8, 0, 0);
+	private Vector3 startPosNext = new Vector3(8 + 16, 0, 0);
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentTrack = Instantiate(tracks[Random.Range(0, tracks.Length)], this.transform);
+        currentTrack.transform.position = startPosCurrent;
+        nextTrack = instantiateNextTrack();
+    }
+
+    private GameObject instantiateNextTrack()
+    {
+        var n = Instantiate(tracks[Random.Range(0, tracks.Length)]);
+        n.transform.position = startPosNext;
+		return n;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+		offset = offset + speed;
+		if(offset > 16) {
+			offset -= 16;
+			Destroy(currentTrack);
+			currentTrack = nextTrack;
+			nextTrack = instantiateNextTrack();
+		}
+		currentTrack.transform.position = new Vector3(startPosCurrent.x - offset, currentTrack.transform.position.y, currentTrack.transform.position.z);
+		nextTrack.transform.position = new Vector3(startPosNext.x - offset, nextTrack.transform.position.y, nextTrack.transform.position.z);
+		
+	}
+}
--- Assets/Scripts/CameraSizeFromRatio.cs
+++ Assets/Scripts/CameraSizeFromRatio.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class CameraSizeFromRatio : MonoBehaviour {
 
-	public float height = 10f;
+	public float width = 8f;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,7 +13,7 @@ public class CameraSizeFromRatio : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		Camera.main.orthographicSize = height / Camera.main.aspect;
-		Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, - Camera.main.orthographicSize, Camera.main.transform.position.z);
+		Camera.main.orthographicSize = width / Camera.main.aspect;
+		Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
 	}
 }
