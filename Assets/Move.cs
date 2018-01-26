594634bc3cc15b58b7270f61ac574a583645a240
Arth-kun <arthur.couge@gmail.com>
2018-01-27 00:00:26 +0100

    car moving up and down

---
+++ Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float playerId;	
+	private Rigidbody2D rb;
+	private Vector2 movingVector;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movingVector = new Vector2(0, Input.GetAxis("Vertical"));				
+		rb.velocity = movingVector * 5;
+	}
+}
