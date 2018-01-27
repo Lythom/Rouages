5ea0fc96f29006c1b6269d547833fef01ffd7f50
Samuel Bouchet <samuel.bouchet@gmail.com>
2018-01-27 17:49:17 +0100

    Fix collisions + add sounds

--- Assets/OnCollisionBoom.cs
+++ Assets/OnCollisionBoom.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class OnCollisionBoom : MonoBehaviour {
+public class OnCollisionBoom : MonoBehaviour
+{
     private AudioSource audioSource;
 
     // Use this for initialization
-    void Start () {
-		audioSource = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        audioSource = GetComponents<AudioSource>().Where(a => a.clip != null).First();
+    }
 
-	private void OnCollisionEnter2D(Collision2D other) {
-		audioSource.Play();
-		audioSource.pitch = Random.Range(2, 3);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+		if (other.relativeVelocity.magnitude < 1.5) return;
+
+        audioSource.Stop();
+        audioSource.pitch = Random.Range(2, 3.3f);
+		Debug.Log(other.relativeVelocity.magnitude);
+		audioSource.volume = Mathf.Min(other.relativeVelocity.magnitude / 20, 0.35f);
+        audioSource.Play();
+    }
 }
