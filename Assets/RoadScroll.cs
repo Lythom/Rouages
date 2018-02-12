eddc7b57373ff3d97b09b4ec22d997e48fa554ae
Samuel BOUCHET <contact@samuel-bouchet.fr>
2018-02-12 09:16:53 +0100

    Desert tracks

--- Assets/RoadScroll.cs
+++ Assets/RoadScroll.cs
@@ -1,94 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
 using System.Text.RegularExpressions;
-public class RoadScroll : MonoBehaviour
-{
+using UnityEngine;
+public class RoadScroll : MonoBehaviour {
 
-	public static float TRACK_WIDTH = 32f;
+    public static float TRACK_WIDTH = 32f;
 
     public double speed = 0.15f;
 
     public GameObject[] tracks;
 
     public GameObject tutoTrack;
-	
-	[Tooltip("Piloté automatiquement, ne pas modifier")]
-	public double offset = 0f;
-	[Tooltip("Piloté automatiquement, ne pas modifier")]
-	// TODO : move to Party and use A delegate event to inform of offsetChange
-	public float trackFinishedCount = 0f;
+
+    [Tooltip ("Piloté automatiquement, ne pas modifier")]
+    public double offset = 0f;
+    [Tooltip ("Piloté automatiquement, ne pas modifier")]
+    // TODO : move to Party and use A delegate event to inform of offsetChange
+    public float trackFinishedCount = 0f;
     public bool tutorial;
 
     private GameObject[] compatibleTracks;
     private GameObject currentTrack = null;
     private GameObject nextTrack = null;
 
-    private Vector3 startPosCurrent = new Vector3(TRACK_WIDTH / 4, 0, 0);
-    private Vector3 startPosNext = new Vector3(TRACK_WIDTH / 4 + TRACK_WIDTH, 0, 0);
-
+    private Vector3 startPosCurrent = new Vector3 (TRACK_WIDTH / 4, 0, 0);
+    private Vector3 startPosNext = new Vector3 (TRACK_WIDTH / 4 + TRACK_WIDTH, 0, 0);
 
     // Use this for initialization
-    void Start()
-    {   
-        currentTrack = Instantiate(tutoTrack);
+    void Start () {
+        currentTrack = Instantiate (tutoTrack);
         currentTrack.transform.position = startPosCurrent;
-        nextTrack = instantiateNextTrack();
+        nextTrack = instantiateNextTrack ();
     }
 
-    private GameObject instantiateNextTrack()
-    {
-        if (! tutorial)
-        {
-            var currentTrackNumber = Regex.Match(currentTrack.name, @"\d+").Value;            
-            if (int.Parse(currentTrackNumber) == 1)
-            {
+    private int getOut (string trackName) {
+        return int.Parse (trackName[trackName.Length - 1].ToString ());
+    }
+    private int getIn (string trackName) {
+        return int.Parse (trackName[trackName.Length - 2].ToString ());
+    }
+
+    private GameObject instantiateNextTrack () {
+        if (!tutorial) {
+            if (currentTrack.name == tutoTrack.name) {
                 // Starting tile is the first of the array
-                var n = Instantiate(tracks[0], this.transform);
+                var n = Instantiate (tracks[0], this.transform);
+                n.name = n.name.Replace ("(Clone)", "");
                 n.transform.position = startPosNext;
                 return n;
-            } else
-            {
-                var currentTrackNumberArray = currentTrackNumber.ToCharArray();
+            } else {
+                int output = getOut (currentTrack.name);
+                compatibleTracks = tracks.Where (item => {
+                    var input = getIn (item.name);
+                    return (input & output) == output; // input must cover at least all the outputs
+                }).ToArray ();
 
-                compatibleTracks = tracks.Where(item => {
-                    var itemNumber = Regex.Match(item.name, @"\d+").Value;
-                    var itemNumberArray = itemNumber.ToCharArray();
-                    var output = int.Parse(currentTrackNumberArray[1].ToString());
-                    var input = int.Parse(itemNumberArray[0].ToString());
-
-                    return output == input;
-                }).ToArray();
-                
-                var n = Instantiate(compatibleTracks[Random.Range(0, compatibleTracks.Length)]);
+                GameObject randomTrack = compatibleTracks[Random.Range (0, compatibleTracks.Length)];
+                var n = Instantiate (randomTrack);
+                n.name = n.name.Replace ("(Clone)", "");
                 n.transform.position = startPosNext;
                 return n;
             }
-        } else
-        {
-            var n = Instantiate(tutoTrack);
+        } else {
+            var n = Instantiate (tutoTrack);
+            n.name = n.name.Replace ("(Clone)", "");
             n.transform.position = startPosNext;
-            return n; 
+            return n;
         }
-            
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate () {
         offset = offset + speed;
-        if (offset > TRACK_WIDTH)
-        {
+        if (offset > TRACK_WIDTH) {
             offset -= TRACK_WIDTH;
-            Destroy(currentTrack);
+            Destroy (currentTrack);
             currentTrack = nextTrack;
-            nextTrack = instantiateNextTrack();
-            if(! tutorial)
-			    trackFinishedCount ++;
+            nextTrack = instantiateNextTrack ();
+            if (!tutorial)
+                trackFinishedCount++;
         }
-        currentTrack.transform.position = new Vector3(startPosCurrent.x - (float)offset, currentTrack.transform.position.y, currentTrack.transform.position.z);
-        nextTrack.transform.position = new Vector3(startPosNext.x - (float)offset, nextTrack.transform.position.y, nextTrack.transform.position.z);
+        currentTrack.transform.position = new Vector3 (startPosCurrent.x - (float) offset, currentTrack.transform.position.y, currentTrack.transform.position.z);
+        nextTrack.transform.position = new Vector3 (startPosNext.x - (float) offset, nextTrack.transform.position.y, nextTrack.transform.position.z);
 
     }
 }
---
+++ Assets/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public bool alsoWave = false;
+
+	private float i = 0;
+	// Update is called once per frame
+	void Update () {
+		i+=0.1f;
+		transform.Rotate (0, 0, -4f + (alsoWave ? 2f : 0));
+		if (alsoWave) {
+			transform.localPosition = new Vector3 (0, Mathf.Cos (i) * 0.4f + 0.2f, 0);
+		}
+	}
+}
